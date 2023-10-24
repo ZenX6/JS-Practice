5a86254715df6da642ecf140585ab6baaafd0260
Ken <dangken55@gmail.com>
2023-10-23 18:27:06 -0700

    Pyramid-Array

---
+++ pyramid.js
@@ -0,0 +1,45 @@
+// For example, given 2, 3, 7, 5, 9 as the base, we should construct this pyramid:
+//
+//           85
+//         37  48
+//       15  22  26
+//    5   10   12   14
+//  2   3    7    5    9
+//
+//
+function pyramidArray(base){
+    let pyramid = [base]
+
+    while (base.length > 1){
+        let newRow = [];
+
+        for (let i = 0; i < base.length - 1; i++){
+            newRow.push(base[i] + base[i + 1]);
+        }
+
+        pyramid.unshift(newRow);
+        base = newRow;
+    }
+    return pyramid;
+}
+
+
+
+let p1 = pyramidArray([2, 3, 7, 5, 9]);
+console.log(p1);
+// [
+//   [ 85 ],
+//   [ 37, 48 ],
+//   [ 15, 22, 26 ],
+//   [ 5, 10, 12, 14 ],
+//   [ 2, 3, 7, 5, 9 ]
+// ]
+
+let p2 = pyramidArray([2, 2, 2, 2]);
+console.log(p2);
+// [
+//   [ 16 ],
+//   [ 8, 8 ],
+//   [ 4, 4, 4 ],
+//   [ 2, 2, 2, 2 ]
+// ]
